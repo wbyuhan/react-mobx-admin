2cf378de1ab95a5f4651cfb86e98596b4bfd1300
chunsheng.li <chunsheng.li@99bill.com>
2021-06-01 20:44:37 +0800

    refactor: 修改路由渲染

--- src/router/router.ts
+++ src/router/router.ts
@@ -4,18 +4,46 @@ const router:Array<any> = [
         redirect: '/app',
       },
       {
-        name: 'app',
+        name: 'routes1',
         path: '/app',
         icon: 'UserOutlined',
-        component: './App',
         access: true,
         routes:[
           {
-            path: '/demand',
+            path: '/routes2',
             redirect: '/demand/underway',
           },
+          {
+            name: 'demand',
+            path: '/demand',
+            icon: 'UserOutlined',
+            component: './Demand/Undecided',
+          },
+          {
+            name: 'test',
+            path: '/test',
+            icon: 'UserOutlined',
+            routes:[
+              {
+                path: '/routes3',
+                redirect: '/demand/test',
+              },
+              {
+                name: 'routes3',
+                path: '/test',
+                icon: 'UserOutlined',
+                component: './Demand/test',
+              }
+            ]
+          }
         ]
-      }
+      },
+      {
+        name: 'index',
+        path: '/index',
+        icon: 'UserOutlined',
+        component: './Demand/index',
+      },
 ]
 
 export default router
--- src/router/util/index.ts
+++ src/router/util/index.ts
@@ -1,43 +1,35 @@
 import { lazy } from 'react'
 
-let routesItem
-let routeComponent
-
-const deep = () => {
-    const routes:any = []
-    let component
-    while (routesItem && routeComponent) {
-        routesItem.forEach((item: { routes: any; component: string }) => {
-            routes.push({
-                ...item,
-                component: item.component && lazy(() => import(`@/pages${item.component.replace(".","")}`))
-            })
-        })
-        component = lazy(() => import(`@/pages${routeComponent.replace(".","")}`))
-    }
-    return {
-        routes,
-        component
-    }
-}
-
+let componentRoutes: any = []
 const routerRender = (router) => {
-
-    const component  = router.map(item => {
-        
-        routesItem = item.routes
-        routeComponent = item?.component
-        return {
-            ...item,
-            exact:true,
-            ...deep()
-            // component: 
+    componentRoutes = router.map(item => {
+        if(item.routes && !item.component){
+            return {
+                ...item,
+                routes:routerRender(item.routes)
+            }
         }
-    }).filter(item => !!item.component);
-    console.log('%c 🍬 component: ', 'font-size:20px;background-color: #B03734;color:#fff;', component);
-    return component
-    
+        if(!item.routes && item.component){
+            return {
+                ...item,
+                component: lazy(() => import(`@/pages${item.component.replace(".","")}`))
+                
+            }
+        }
+        if(item.routes && item.component){
+            return {
+                ...item,
+                routes:routerRender(item.routes),
+                component: lazy(() => import(`@/pages${item.component.replace(".","")}`))
+            }
+        }
+        
+        return {
+            ...item
+        }
+        
+    })
+    return componentRoutes
 }
-
 
 export default routerRender
